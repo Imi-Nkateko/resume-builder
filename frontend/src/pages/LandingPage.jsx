d9786536687e2b5420ebb89c56694131f68418a4
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-05-13 23:26:36 +0200

    create Header ui

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,15 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
-
-
 const LandingPage = () => {
   const navigate = useNavigate();
   const [openAuthModal, setOpenAuthModal] = useState(false);
   // const [currentPage, setCurrentPage] = useState("login");
 
   return (
-   <div>csc</div>
+    <div>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+          <button onClick={() => setOpenAuthModal(true)} className="btn btn-outline btn-secondary">
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
