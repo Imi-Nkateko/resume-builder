963e1165e49dd9d2ff9caed80695649407326153
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-05-14 00:28:41 +0200

    add hero section ui

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -4,20 +4,30 @@ import { useState } from "react";
 const LandingPage = () => {
   const navigate = useNavigate();
   const [openAuthModal, setOpenAuthModal] = useState(false);
-  // const [currentPage, setCurrentPage] = useState("login");
+  const [currentPage, setCurrentPage] = useState("login");
+
+  const handleCTA = () => {};
 
   return (
-    <div>
+    <div className="w-full min-h-full">
       <div className="navbar bg-base-100 shadow-sm">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">daisyUI</a>
         </div>
         <div className="flex-none">
-          <button onClick={() => setOpenAuthModal(true)} className="btn btn-outline btn-secondary">
+          <button
+            onClick={() => setOpenAuthModal(true)}
+            className="btn btn-outline btn-secondary"
+          >
             Login
           </button>
         </div>
       </div>
+
+      {/* Hero Section */}
+      <button onClick={handleCTA} className="btn btn-outline btn-secondary">
+        Create Resume
+      </button>
     </div>
   );
 };
